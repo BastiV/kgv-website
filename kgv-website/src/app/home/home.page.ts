b5f431f873101fcb24f6c74ee12ef8a5dc31900f
Bastian Voigt <voigt_bastian@arcor.de>
2024-03-07 16:52:47 +0100

    * initialize split pane and set primary colors

--- kgv-website/src/app/home/home.page.ts
+++ kgv-website/src/app/home/home.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSplitPane, IonMenu, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonSplitPane, IonHeader, IonToolbar, IonTitle, IonContent, IonMenu, IonButtons, IonMenuButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+  }
+
 }
